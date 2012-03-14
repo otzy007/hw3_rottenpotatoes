f02826b5522c6819215c369668d18b42d0d75f0f
Andrei G <oz@oprod.net>
2012-03-14 22:07:31 +0200

    modified:   features/filter_movie_list.feature 	modified:   features/step_definitions/movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -25,7 +25,7 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
